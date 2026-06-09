4e191db94e163cccd0381802d2abe09c640fe16e
lychar20 <charly?mampuya@yahoo.fr>
2026-06-09 16:16:47 +0200

    chore: remove temp clearRooms script

--- backend/clearRooms.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
-
-await mongoose.connect(process.env.SECRET_DB);
-const result = await mongoose.connection.collection('rooms').deleteMany({});
-console.log('Rooms supprimées:', result.deletedCount);
-await mongoose.disconnect();
-process.exit(0);
